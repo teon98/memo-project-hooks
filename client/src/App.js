0ba078b9e6eea9183846155632f234822c92cde6
Chanran33 <k55416325@gmail.com>
2021-10-01 23:40:10 +0900

    흑흑

--- client/src/App.js
+++ client/src/App.js
@@ -1,77 +1,77 @@
-import React, { memo } from 'react';
-import './App.css';
-import plus from './plus.png';
-import Modal from './components/Modal';
-import ReModal from './components/ReModal';
+import React, { memo } from "react";
+import "./App.css";
+import plus from "./plus.png";
+import Modal from "./components/Modal";
+import ReModal from "./components/ReModal";
 
 class App extends React.Component {
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
       isModalOpen: false,
       reModalOpen: false,
-      memos:[
+      memos: [
         {
           title: "첫번째 메모",
           author: "ty",
-          content: "필라테스 4시"
+          content: "필라테스 4시",
         },
         {
           title: "두번째 메모",
           author: "ty",
-          content: "웨이트 OT 6시"
+          content: "웨이트 OT 6시",
         },
       ],
-      clickmemo:{
-        index:'',
-        title:'',
-        author:'',
-        content:'',
-      }
+      clickmemo: {
+        index: "",
+        title: "",
+        author: "",
+        content: "",
+      },
     };
   }
 
   openModal = () => {
-    this.setState({isModalOpen: true});
-  }
+    this.setState({ isModalOpen: true });
+  };
 
   closeModal = () => {
-    this.setState({ isModalOpen: false});
-  }
+    this.setState({ isModalOpen: false });
+  };
 
   //수정할 메모는 모달창을 열 때 입력되어있는 state대로 올라갈 수 있도록 한다.
   reopenModal = (index) => {
     //reModal창을 열어주자
     this.setState({
-      reModalOpen: true, 
-      clickmemo:{
-        index:index,
-        title:this.state.memos[index].title,
-        author:this.state.memos[index].author,
-        content:this.state.memos[index].content,
-      }
+      reModalOpen: true,
+      clickmemo: {
+        index: index,
+        title: this.state.memos[index].title,
+        author: this.state.memos[index].author,
+        content: this.state.memos[index].content,
+      },
     });
-  }
+  };
 
   recloseModal = () => {
-    this.setState({reModalOpen: false});
-  }
+    this.setState({ reModalOpen: false });
+  };
 
   handleCreate = (new_memo) => {
     console.log(new_memo);
     let memos = this.state.memos;
-    this.setState({memos: [...memos, new_memo]});
-  }
+    this.setState({ memos: [...memos, new_memo] });
+  };
 
   handleUpdate = (id, change_memo) => {
     console.log(id);
     console.log(change_memo);
     let memos = this.state.memos;
     this.setState({
-      memos: memos.map((memos,index) => {
-        if(index === id){
-          console.log(index + '/' + id);
-          return {id, ...change_memo};
+      memos: memos.map((memos, index) => {
+        if (index === id) {
+          console.log(index + "/" + id);
+          return { id, ...change_memo };
         }
         return memos;
       }),
@@ -81,47 +81,58 @@ class App extends React.Component {
   handleRemove = (id) => {
     let memos = this.state.memos;
     this.setState({
-      memos: memos.filter((memo,index) => index !== id),
+      memos: memos.filter((memo, index) => index !== id),
     });
   };
 
-  render(){
-    return(
-      <div className='container'>
-        <div className='App'>
-            <h1>메모장</h1>
-            <br/>
-            <br/>
-            <table>
-              <tbody>
-                <tr className="trList">
-                  {this.state.memos.map((memo,index) =>
-                    <td className="cell" key={index} onClick={()=>this.reopenModal(index)}>
-                      <div className="inner">
-                        <h2>{memo.title}</h2>
-                        <h5>{memo.author}</h5>
-                        <br/>
-                        <br/>
-                        <h4>{memo.content}</h4>
-                        <br/>
-                      </div>
-                    </td>
-                  )}
-                  <td className='cell'>
-                    <div className="inner" onClick={this.openModal}>
-                      <img src={plus} className='picture' alt='logo' />
+  render() {
+    return (
+      <div className="container">
+        <div className="App">
+          <h1>메모장</h1>
+          <br />
+          <br />
+          <table>
+            <tbody>
+              <tr className="trList">
+                {this.state.memos.map((memo, index) => (
+                  <td
+                    className="cell"
+                    key={index}
+                    onClick={() => this.reopenModal(index)}
+                  >
+                    <div className="inner">
+                      <h2>{memo.title}</h2>
+                      <h5>{memo.author}</h5>
+                      <br />
+                      <br />
+                      <h4>{memo.content}</h4>
+                      <br />
                     </div>
                   </td>
-                </tr>
-              </tbody>
-            </table>
-            <main className='App'>
-              <Modal isOpen={this.state.isModalOpen} close={this.closeModal} 
-                    onCreate={this.handleCreate}/>
-              <ReModal reOpen={this.state.reModalOpen} reclose={this.recloseModal} 
-                    data={this.state.clickmemo} onUpdate={this.handleUpdate}
-                    onRemove = {this.handleRemove}/>
-            </main>
+                ))}
+                <td className="cell">
+                  <div className="inner" onClick={this.openModal}>
+                    <img src={plus} className="picture" alt="logo" />
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
+          <main className="App">
+            <Modal
+              isOpen={this.state.isModalOpen}
+              close={this.closeModal}
+              onCreate={this.handleCreate}
+            />
+            <ReModal
+              reOpen={this.state.reModalOpen}
+              reclose={this.recloseModal}
+              data={{ ...this.state.clickmemo }} //딥카피
+              onUpdate={this.handleUpdate}
+              onRemove={this.handleRemove}
+            />
+          </main>
         </div>
       </div>
     );
--- client/src/components/ReModal.js
+++ client/src/components/ReModal.js
@@ -1,84 +1,95 @@
-import React, { Component } from 'react';
-import './Modal.scss';
+import React, { PureComponent } from "react";
+import "./Modal.scss";
 
+class ReModal extends PureComponent {
+  //modal이 붙어있어서 memo창을 바로 열었을 때, 지정한 state값이 뜨지 않는다.
+  //modal창이라는 상태를 history에 넣어 pupState를 통해 불러오거나
+  //라우터를 만들어 수정 입력시 reload시키는 방법이 있을 것 같다.
+  //우리가 하는 memo프로젝트는 이 point가 중요한게 아니니 넘어가자
 
-class ReModal extends Component {
-    //modal이 붙어있어서 memo창을 바로 열었을 때, 지정한 state값이 뜨지 않는다.
-    //modal창이라는 상태를 history에 넣어 pupState를 통해 불러오거나
-    //라우터를 만들어 수정 입력시 reload시키는 방법이 있을 것 같다.
-    //우리가 하는 memo프로젝트는 이 point가 중요한게 아니니 넘어가자
-    state = {
-        title:this.props.data.title,
-        content:this.props.data.content,
-        author:this.props.data.author,
-    }
-  
+  state = {
+    title: this.props.data.title,
+    content: this.props.data.content,
+    author: this.props.data.author,
+  };
+
   handleUpdate = (event) => {
-      event.preventDefault();
-      this.props.onUpdate(this.props.data.index, this.state);
-      this.setState({
-        title:'',
-        content:'',
-        author:''
-      });
-      this.props.reclose();
+    event.preventDefault();
+    this.props.onUpdate(this.props.data.index, this.state);
+    this.setState({
+      title: "",
+      content: "",
+      author: "",
+    });
+    this.props.reclose();
   };
 
   handleChange = (event) => {
-      const {target: {name, value}} = event //비구조화 할당
-      this.setState({[name] : value})
+    const {
+      target: { name, value },
+    } = event; //비구조화 할당
+    this.setState({ [name]: value });
   };
 
   handleRemove = () => {
-    console.log(this.props.data.index)
+    console.log(this.props.data.index);
     this.props.onRemove(this.props.data.index);
     this.props.reclose();
-  }
+  };
 
   render() {
     //props : 부모 컴포넌트로부터 가져오는 상태 값
-    const { reOpen, reclose} = this.props;
+    const { reOpen, reclose } = this.props;
+    console.log(this.props);
 
     return (
-        <div>
-            {
-                reOpen?
-                //모달 창이 열려 있다면
-                <React.Fragment>
-                    <div className="Modal-verlay" onClick={reclose} />
-                    <div className="Modal">
-                        <h1 className="title">메모를 수정하세요!</h1>
-                        <form onSubmit = {this.handleUpdate}>
-                            <div className="content">
-                                <h4>
-                                    <input type='text' placeholder="아이디를 입력하세요"
-                                            name='author' value={this.state.author}
-                                            onChange={this.handleChange}>
-                                            </input>
-                                </h4>
-                                <br/>
-                                <h4>
-                                    <input type='text' placeholder="제목을 입력하세요"
-                                            name="title" value={this.state.title}
-                                            onChange={this.handleChange}></input>
-                                </h4>
-                                <textarea name="content" value={this.state.content} 
-                                            onChange={this.handleChange}>
-                                </textarea>
-                            </div>
-                            <div className="button-wrap">
-                                <button type="submit">
-                                    <p>수정하기</p>
-                                </button>
-                                <button type="button" onClick={this.handleRemove}>
-                                    <p>삭제하기</p>
-                                </button>
-                            </div>
-                        </form>
-                    </div>
-                </React.Fragment> : null
-            }
-        </div>
+      <div>
+        {reOpen && (
+          //모달 창이 열려 있다면
+          <React.Fragment>
+            <div className="Modal-verlay" onClick={reclose} />
+            <div className="Modal">
+              <h1 className="title">메모를 수정하세요!</h1>
+              <form onSubmit={this.handleUpdate}>
+                <div className="content">
+                  <h4>
+                    <input
+                      type="text"
+                      placeholder="아이디를 입력하세요"
+                      name="author"
+                      value={this.state.author}
+                      onChange={this.handleChange}
+                    ></input>
+                  </h4>
+                  <br />
+                  <h4>
+                    <input
+                      type="text"
+                      placeholder="제목을 입력하세요"
+                      name="title"
+                      value={this.state.title}
+                      onChange={this.handleChange}
+                    ></input>
+                  </h4>
+                  <textarea
+                    name="content"
+                    value={this.state.content}
+                    onChange={this.handleChange}
+                  ></textarea>
+                </div>
+                <div className="button-wrap">
+                  <button type="submit">
+                    <p>수정하기</p>
+                  </button>
+                  <button type="button" onClick={this.handleRemove}>
+                    <p>삭제하기</p>
+                  </button>
+                </div>
+              </form>
+            </div>
+          </React.Fragment>
+        )}
+      </div>
     );
   }
 }
